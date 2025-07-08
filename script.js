a55338402d9ba378ef03107de195e36c68a6bc47
Manish8000pro <manishkumawat6227@gmail.com>
2025-07-08 22:18:01 +0530

    song list

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 console.log("lets write the javascript")
 
 async function getsongs() {
-    let a = await fetch("http://127.0.0.1:5500/songs/")
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    console.log(as)
-    let songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split("/song")[1])
-        }
+  let a = await fetch("http://127.0.0.1:5500/songs/")
+  let response = await a.text();
+  let div = document.createElement("div")
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a")
+  console.log(as)
+  let songs = []
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/song")[1])
     }
-    return songs
+  }
+  return songs
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -25,14 +25,14 @@ async function main() {
   let songs = await getsongs();
   console.log(songs);
 
-  let songUL = document.querySelector(".songlist ul"); // ✅ fixed case
+  let songUL = document.querySelector(".songlist ul");
 
   for (const song of songs) {
     let li = document.createElement("li");
     li.textContent = song;
     songUL.appendChild(li);
-    
-    
+
+
   }
 
   let audio = new Audio(songs[0]);
@@ -42,5 +42,7 @@ async function main() {
     console.log(audio.duration, audio.currentSrc, audio.currentTime);
   });
 }
+
+
 
 
